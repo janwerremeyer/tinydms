c8c9a97de1d237caa50fef17fdcee1e5aae7cd74
Jan Werremeyer <jan@werremeyer.me>
2023-06-20 19:27:40 +0200

    Add error converter

---
+++ src/lib/validation/fieldErrorConverter.ts
@@ -0,0 +1,32 @@
+import { HttpException, HttpStatus, ValidationError } from "@nestjs/common";
+
+export function ConvertToFieldErrors(validationErrors: Array<ValidationError>) {
+  const fieldErrors = new Map<string, Set<string>>();
+
+  for (const validationError of validationErrors) {
+    const fieldName = validationError.property;
+    const messages = new Set<string>();
+
+    for (const [, v] of Object.entries(validationError.constraints)) {
+      messages.add(v);
+    }
+
+    fieldErrors.set(fieldName, messages);
+  }
+
+  return fieldErrors;
+}
+
+export function ConvertFieldErrorsToHttpException(fieldErrors: ReturnType<typeof ConvertToFieldErrors>): HttpException {
+  const serializableFieldErrors = Array.from(fieldErrors).map(([k, v]) => ({
+    field: k,
+    messages: Array.from(v),
+  }));
+
+  return new HttpException(
+    {
+      fieldErrors: serializableFieldErrors,
+    },
+    HttpStatus.BAD_REQUEST,
+  );
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  ConvertFieldErrorsToHttpException,
+  ConvertToFieldErrors,
+} from './lib/validation/fieldErrorConverter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (validationErrors) => {
+        const fieldErrors = ConvertToFieldErrors(validationErrors);
+        return ConvertFieldErrorsToHttpException(fieldErrors);
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
