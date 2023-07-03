47b1ea86ce92379ec6a5788d7ca88dea70dd98ab
Jan Werremeyer <jan@werremeyer.me>
2023-07-03 21:18:08 +0200

    Add happy path implementation for file storage and preview

--- backend-service/src/app.module.ts
+++ backend-service/src/app.module.ts
@@ -6,6 +6,8 @@ import {KeycloakConnectModule} from "nest-keycloak-connect";
 import {KeycloakConfigService} from "./config/keycloak-config.service";
 import {ProductsModule} from './products/products.module';
 import {ConfigModule} from "@nestjs/config";
+import { StorageModule } from './storage/storage.module';
+import { PreviewModule } from './preview/preview.module';
 
 const KeycloakConnectModuleInstance = KeycloakConnectModule.registerAsync({
     useExisting: KeycloakConfigService,
@@ -13,7 +15,7 @@ const KeycloakConnectModuleInstance = KeycloakConnectModule.registerAsync({
 })
 
 @Module({
-    imports: [ConfigModule.forRoot(), KeycloakConnectModuleInstance, ProductsModule],
+    imports: [ConfigModule.forRoot(), KeycloakConnectModuleInstance, ProductsModule,  StorageModule, PreviewModule],
     controllers: [AppController],
     providers: [
         AppService
---
+++ backend-service/src/preview/preview.controller.ts
@@ -0,0 +1,37 @@
+import {Controller, Get, Param, Res, StreamableFile} from '@nestjs/common';
+import {PdfpreviewService} from "./pdfpreview.service";
+import {StorageService} from "../storage/storage.service";
+
+@Controller('preview')
+export class PreviewController {
+
+    constructor(private previewService: PdfpreviewService, private storageService: StorageService) {
+    }
+
+
+
+
+
+
+
+    @Get("thumbnail/:key")
+    async thumbnail(@Param() {key}: { key: string },  @Res({passthrough: true}) res: Response) {
+        const s3Respones = await this.storageService.read(key)
+        // const buffer = Buffer.from(await s3Respones.Body.transformToString())
+        const byteArray = await s3Respones.Body.transformToByteArray()
+
+        const buffer =  Buffer.from(byteArray.buffer, byteArray.byteOffset, byteArray.byteLength)
+        const preview = await this.previewService.thumbnail(buffer)
+
+        console.log(preview)
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        res.set({
+            'Content-Type': "image/png"
+        });
+
+        return  new StreamableFile(preview)
+        // return {"ok": true}
+    }
+}
